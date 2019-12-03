6195d61f38abc539494313ef5e2d89e380a3829d
cchoimin91 <cchoimin1991@gmail.com>
2019-12-03 23:39:55 +0900

    SkillTest 1

--- src/com/cm/Main.java
+++ src/com/cm/Main.java
@@ -3,6 +3,7 @@ package com.cm;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("결과값>>>>>");
+        new SkillTestLevel1();
     }
 }
